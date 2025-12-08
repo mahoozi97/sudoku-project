38f8d832c7c3a4e6c074bc1aa6ef75a3a3ca3189
Ali Hasan <mahoozi97@icloud.com>
2025-12-08 20:31:40 +0300

    Add timer functionality and high score tracking to Sudoku game

--- app.js
+++ app.js
@@ -4,28 +4,105 @@ let selectedNumber;
 let message;
 let mistakesCount = 0;
 
+let time = {
+  seconds: 0,
+  minutes: 0,
+  hours: 0,
+};
+let timeMessage;
+let scoreLog = [];
+let timeup;
+let fetchedLog = false;
+
 const cells = document.querySelectorAll(".js-cell");
 const buttons = document.querySelectorAll(".button-container button");
 const messageEl = document.querySelector(".message");
 const mistakesEl = document.querySelector(".mistakes");
 const restartBtn = document.querySelector(".restart");
+const timeEl = document.querySelector(".time");
+const scoreListEl = document.querySelector(".score-list");
+const highScoreBtn = document.querySelector(".high-score-btn");
+
+function init() {
+  timeEl.textContent = "00:00:00";
+  timeup = setInterval(timeCountUp, 1000);
+  mistakesText();
+}
+init();
+
+function timeCountUp() {
+  let seconds;
+  let minutes;
+  let hours;
+
+  time.seconds++;
+
+  if (time.seconds === 60) {
+    time.minutes++;
+    time.seconds = 0;
+  }
+
+  if (time.minutes === 60) {
+    time.hours++;
+    time.minutes = 0;
+  }
+
+  if (time.seconds <= 9) {
+    seconds = `0${time.seconds}`;
+  } else {
+    seconds = `${time.seconds}`;
+  }
+
+  if (time.minutes <= 9) {
+    minutes = `0${time.minutes}`;
+  } else {
+    minutes = `${time.minutes}`;
+  }
+
+  if (time.hours <= 9) {
+    hours = `0${time.hours}`;
+  } else {
+    hours = `${time.hours}`;
+  }
+
+  timeMessage = `${hours}:${minutes}:${seconds}`;
+  timeEl.textContent = timeMessage;
+}
+
+function saveScoreList(timeMessage) {
+  scoreLog.push(timeMessage);
+  localStorage.setItem("score", JSON.stringify(scoreLog));
+}
+
+function fetchScoreList() {
+  const highScores = JSON.parse(localStorage.getItem("score"));
+
+  highScores.forEach((log) => {
+    let list = document.createElement("li");
+    list.textContent = log;
+    scoreListEl.appendChild(list);
+    fetchedLog = true
+  });
+}
 
 function mistakesText() {
   mistakesEl.textContent = `🔴 ${mistakesCount} / 4`;
 }
-mistakesText();
 
 function showWinMessage() {
-  if (message !== undefined) {
+  if (message === "You Win!") {
     messageEl.textContent = message;
     messageEl.style.color = "green";
     restartBtn.classList.remove("hidden");
+    clearInterval(timeup);
+    saveScoreList(timeMessage);
     disableButtons();
   }
 }
 
 function showLoseMessage() {
   if (mistakesCount === 4) {
+    clearInterval(timeup);
     disableButtons();
     messageEl.textContent = "You Lose!";
     messageEl.style.color = "red";
@@ -93,6 +170,12 @@ cells.forEach((cell) => {
 
     showWinMessage();
   });
+});
+
+highScoreBtn.addEventListener("click", () => {
+  if (!fetchedLog) {
+    fetchScoreList();
+  }
 });
 
 restartBtn.addEventListener("click", () => {
